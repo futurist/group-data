97956191a9c454295bde7f4279059a4decfe9708
futurist <jamesyang999@gmail.com>
2017-04-19 12:08:21 +0800

    fix to int bug

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ var data2 = {
     }, {
       "item": "aa",
       "color": "1",
-      "qty": 32,
+      // "qty": 32,
       // x:{y:1}
     }, {
       "item": "aa",
@@ -129,7 +129,7 @@ var stage2 = {
   },
   // _id: null,
   asdf: {$sum: '$bb.cc.qty'},
-  count: {$sum: 1},
+  // count: {$sum: 1},
   count2: {$sum: 1.5},
 }
 
@@ -157,9 +157,9 @@ function v2(data, stage){
           if(!(i in entry)) entry[i] = 0
           if(typeof keyPath=='string') {
             const arr = getDataInPath(data, currentPath, keyPath)
-            entry[i] += arr[0]
+            entry[i] += arr[0]||0
           } else if(typeof keyPath == 'number') {
-            entry[i] += keyPath
+            entry[i] += keyPath||0
           }
           break
         }
